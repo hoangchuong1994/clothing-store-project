f1914c8f7c42e4db3fdcb16f4a27828b87685db5
hoangchuong300494 <hoangvanchuong0917023924@gmail.com>
2026-03-29 16:43:44 +0700

    first commit

---
+++ src/app/[locale]/(home)/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function LocalePage() {
+  return (
+    <main className="w-full bg-linear-to-b from-[#020617] via-[#0b1120] to-[#020617]">
+      <h1>home page</h1>
+    </main>
+  );
+}
